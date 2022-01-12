6d6ec10f7a93bdb6c9f7c2433209ad6c50b3d10c
juliapinheiro42 <juliapinheiro.142@gmail.com>
2022-01-12 00:40:43 -0300

    segundo projeto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 import './App.css';
+class App extends Component{
 
-function App() {
+  constructor(props){
+     super(props);
+     this.state = {
+       numero: 0,
+       botao: ''
+     }
+     this.timer = null;
+     this.vai = this.vai.bind(this);
+     this.limpar = this.limpar.bind(this);
+  }
+  vai(){
+    let state = this.state;
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'VAI'
+    } else {
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+        
+    },100);
+    state.botao = 'Pausa';
+  }   
+  this.setState(state);
+}
+  limpar(){
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'VAI';
+    this.setState(state);
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={require('./Assets/cronometro.png')} className='Img'/>
+      <a className='timer'>{this.state.numero.toFixed(1)}</a>
+      <div className='areaBtn'>
+        <a className='botao'onClick={this.vai}>{this.state.botao}</a>
+        <a className='botao'onClick={this.limpar}>LIMPAR</a>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
